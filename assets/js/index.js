7530b7bd87ca74a11588cb54d462d1e5154cfe97
Ernesto Gonzalez <ernestoale37@gmail.com>
2024-06-21 16:46:44 -0400

    Finished the weather information component, working on past entries functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 const button = document.getElementById('confirm');
 const entry = document.getElementById('entry');
-
+const cities = JSON.parse(localStorage.getItem('cities'));
 function getInput() {
     var input = '';
     if (entry.value != '') {
@@ -34,47 +34,65 @@ async function currentWeatherData(location) {
     editW.wind = output.wind.speed;
     editW.hum = output.main.humidity;
     var info = document.querySelectorAll('section.info ul li');
-    console.log(info);
-    info[0].innerText = `${location} ${editW.date} ${editW.icon}`;
+    info[0].innerText = `${location} (${editW.date})  `;
+    var icon = info[0].appendChild(document.createElement('img'));
+    icon.setAttribute('src', `https://openweathermap.org/img/w/${editW.icon}.png`);
     info[1].innerText = `Temperature: ${editW.temp} °F`;
-    info[2].innerText = `Wind speed: ${editW.wind} mil/h`;
-    info[3].innerText = `Humidity: ${editW.hum}`;
+    info[2].innerText = `Wind speed: ${editW.wind} MPH`;
+    info[3].innerText = `Humidity: ${editW.hum} %`;
 };
 
 async function forecastWeatherData(location) {
     const data = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&exclude=hourly,minutely&appid=12135702e5f184ed44083de9995aa360`);
     const output = await data.json();
-    var x = 0;
-    var days = [];
+    var x = 1;
+    var y = 0;
     let date2 = '';
-    for (i = 0; i < 35; i+=7) {
-        days[x] = {date: "", icon: "", temp: 0, wind: 0, hum: 0};
+    var forecast = document.querySelector('section.dashboard');
+    days = forecast.querySelectorAll('ul');
+    for (i = 6; i < 35; i += 7) {
+        var li = days[y].querySelectorAll('li');
         let date = new Date(output.list[i].dt * 1000);
         date = JSON.stringify(date);
         for (let j = 1; j < 11; j++) {
             date2 += date.charAt(j);
         }
-        days[x].date = date2;
-        days[x].icon = output.list[i].weather[0].icon;
-        days[x].temp = Math.ceil((output.list[i].main.temp - 273.15) * 9/5 + 32);
-        days[x].wind = output.list[i].wind.speed;
-        days[x].hum = output.list[i].main.humidity;
+        li[0].innerText = `${date2}`;
+        li[1].innerText = "";
+        var icon = li[1].appendChild(document.createElement('img'));
+        icon.setAttribute('src', `https://openweathermap.org/img/w/${output.list[i].weather[0].icon}.png`);
+        li[2].innerText = `Temp: ${Math.ceil((output.list[i].main.temp - 273.15) * 9/5 + 32)} °F`;
+        li[3].innerText = `Wind: ${output.list[i].wind.speed} MPH`;
+        li[4].innerText = `Hum: ${output.list[i].main.humidity} %`;
         date2 = '';
         x++;
-    }
-    return days;
+        y++;
+    };
 };
 
-function updateInfo(current) {
-    var info = document.querySelectorAll('section.info ul li');
-    console.log(info);
-    info[0].innerText = `${current.date}`;
-    info[1].innerText = `${current.temp}`;
-    info[2].innerText = `${current.wind}`;
-    info[3].innerText = `${current.hum}`;
+function addEntries(location) {
+    if (cities != null) {
+        cities.push(location);
+        for (i = 0; i < cities.length; i++) {
+            var render = document.getElementById('cities').appendChild(document.createElement('button'));
+            render.setAttribute('style', 'margin-left: 20px; width: 200px; height: 35px; border-radius: 50px; margin-top: 10%;');
+            render.setAttribute('onclick', `{currentWeatherData(${cities[i]}); forecastWeatherData(${cities[i]});}`);
+            render.innerHTML(`${cities[0]}`);
+        };
+    } else {
+        var cities = [];
+        cities.push(location);
+        var render = document.getElementById('cities').appendChild(document.createElement('button'));
+        render.setAttribute('style', 'margin-left: 20px; width: 200px; height: 35px; border-radius: 50px; margin-top: 10%;');
+        render.setAttribute('onclick', `{currentWeatherData(${cities[0]}); forecastWeatherData(${cities[0]});}`);
+        render.innerHTML(`${cities[0]}`);
+    };
+    localStorage.setItem('cities', JSON.stringify(cities));
 };
 
 button.addEventListener('click', function (event) {
     event.preventDefault();
     currentWeatherData(getInput());
+    forecastWeatherData(getInput());
+    addEntries(getInput());
 });
